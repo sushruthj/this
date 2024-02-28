cca63d124caece9b050f5cadc99a87cbb2d6a9be
sushruthj <susruthanvesh@gmail.com>
2024-02-28 19:03:56 +0530

    Update foldertel.py

--- foldertel.py
+++ foldertel.py
@@ -20,8 +20,8 @@ def artist(update: Update, context: CallbackContext):
     mediapath="/mnt"
     #folder_path = os.path.join(home_dir, artist_name, album_name)
     midpath = "Music"
-    subprocess.call('echo @Sne820447 | sudo apt update')
-    folder_path = os.path.join(mediapath, midpath, artist_name, album_name)
+    #subprocess.call('echo @Sne820447 | sudo apt update')
+    folder_path = os.path.join(home_dir, midpath, artist_name, album_name)
 
     try:
         os.makedirs(folder_path)
